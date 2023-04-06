dae459b323b52f8c3ace00f03f9acb25e79aed2e
AllanKirui <nallawilljr@gmail.com>
2023-04-06 12:09:14 +0300

    Add notes on comments

---
+++ 01_comments.php
@@ -0,0 +1,13 @@
+<?php
+// PHP files start with that above opening tag <?php
+// The closing tag is optional, unless there is more code below such as HTML
+
+// Single-line comments like these start with double foward slashes
+
+/*
+Multi-line
+comments
+can be
+written
+like this
+*/
