b374ba60e2b23d9773d929bc796b0a04b9dae721
Allan Kirui <allan.kirui@outlook.com>
2023-05-03 12:27:38 +0300

    Update notes on comments

--- 01_comments.php
+++ 01_comments.php
@@ -1,6 +1,6 @@
 <?php
 // PHP files start with that above opening tag <?php
-// The closing tag is optional, unless there is more code below such as HTML
+// The closing tag is optional, unless there is more code below it such as HTML
 
 // Single-line comments like these start with double foward slashes
 
