b0a02f20cfbc1f7befb95e8df00dfaea1d984106
Allan Kirui <allan.kirui@outlook.com>
2023-05-04 14:06:04 +0300

    Add notes on php data types

---
+++ 03_variables.php
@@ -0,0 +1,15 @@
+<?php
+// ------------ Variables and Data Types -----------
+
+/* 
+  ------------ Data Types in PHP ------------
+
+  1. String - a string is a series of characters surrounded by quotes
+  2. Integer - these are whole numbers
+  3. Float - these are decimal numbers
+  4. Boolean - true or false
+  5. Array - a special variable that can hold multiple values
+  6. Object - a class
+  7. NULL - represents an empty variable
+  8. Resource - a special variable that holds a resource
+*/
